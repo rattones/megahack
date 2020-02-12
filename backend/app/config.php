50685a35e83e977858840b6c2bc563504a30cb2a
Marcelo Ratton <marcelo.ratton@telemont.net>
2020-02-12 11:35:13 -0300

    reseting project router structures

--- backend/app/config.php
+++ backend/app/config.php
@@ -3,18 +3,3 @@
 // default route
 $routes['get']['']                  = 'Welcome';
 
-// project routes
-// product 
-$routes['get']['product']           = 'Product/list';
-$routes['get']['product/:id']       = 'Product/get';
-$routes['post']['product']          = 'Product/create';
-$routes['post']['product/search']   = 'Product/search';
-$routes['put']['product/:id']       = 'Product/set';
-$routes['delete']['product/:id']    = 'Product/delete';
-// category
-$routes['get']['category']          = 'Category/list';
-$routes['get']['category/:id']      = 'Category/get';
-$routes['post']['category']         = 'Category/create';
-$routes['post']['category/search']  = 'Category/search';
-$routes['put']['category/:id']      = 'Category/set';
-$routes['delete']['category/:id']   = 'Category/delete';
