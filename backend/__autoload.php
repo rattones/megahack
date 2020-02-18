c7fb7b69e08b35e7f6521ce2a69dab6c1f644b3d
Marcelo Ratton <marcelo.ratton@telemont.net>
2020-02-18 11:19:19 -0300

    testing

--- backend/__autoload.php
+++ backend/__autoload.php
@@ -1,23 +1,19 @@
 <?php
-// echo '<pre>';
-
 // fixing autoload register
 spl_autoload_register("myAutoload");
 
 function myAutoload ($class)
 {
-    // echo __FILE__.'<br>';
     $class= str_replace('\\', '/', $class);
     $classFile= "./{$class}.php";
+\debug($classFile);
     if (file_exists($classFile)) {
         require_once $classFile;
-        // echo "Loaded: {$classFile}<br>";
     } else {
         $lastDash= strrpos($classFile, '/');
         $classFile= substr($classFile, 0 , $lastDash).'.php';
         if (file_exists($classFile)) {
             require_once $classFile;
-            // echo "Loaded: {$classFile}<br>";
         }
     }
 }
